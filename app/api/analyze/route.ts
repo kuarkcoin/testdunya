4345a056a11b0db46ab5ca855149a5898197b05f
kuarkcoin <30313951+kuarkcoin@users.noreply.github.com>
2025-12-16 11:17:23 +0300

    Update route.ts

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: Request) {
     // 2. Kurulum
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash", // En güvenli model
+      model: "gemini-2.5-flash", // En güvenli model
       generationConfig: {
         responseMimeType: "application/json",
         responseSchema: {
