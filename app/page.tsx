a71ddfd63fbc6d0dc2f9f1589226481ea132762a
kuarkcoin <30313951+kuarkcoin@users.noreply.github.com>
2025-12-18 23:16:53 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Trophy = (props: React.SVGProps<SVGSVGElement>) => (
     <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
   </svg>
 );
-
+ 
 const Book = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
     <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
